2ee5b368a392a03e7cd21a11ba955e698a6565bd
Victor B. Lino <victorbarrosolino@gmail.com>
2023-05-04 00:02:26 -0300

    Revert "fix bug vod timestamp"
    This reverts commit a69230f2316c8b37e1d8de968a259c0f8ef1f161.

--- functions/twitchFunctions.py
+++ functions/twitchFunctions.py
@@ -84,13 +84,9 @@ async def channel_update(data: dict):
 
     if variables.category_name != data['event']['category_name'] and variables.online == True: # If category (or game) change
         
-        if variables.title_stream == twitch.get_videos(user_id=id_streamer)['data'][0]['duration']:
-            timestamp = twitch.get_videos(user_id=id_streamer)['data'][0]['duration'] # Get timestamp in VOD
-            status = f'{variables.streamer_nickname} está jogando: {variables.category_name}\nTempo no VOD: {timestamp}\n\ntwitch.tv/{variables.streamer_nickname}' # Prepare Twitter status
-        else: 
-            status = f'{variables.streamer_nickname} está jogando: {variables.category_name}\n\ntwitch.tv/{variables.streamer_nickname}' # Prepare Twitter status
+        timestamp = twitch.get_videos(user_id=id_streamer)['data'][0]['duration'] # Get timestamp in VOD
         variables.category_name = data['event']['category_name'] # Change the game variable
-        
+        status = f'{variables.streamer_nickname} está jogando: {variables.category_name}\nTempo no VOD: {timestamp}\n\ntwitch.tv/{variables.streamer_nickname}' # Prepare Twitter status
         downloadImageGame(twitch.get_games(names=variables.category_name)['data'][0]['box_art_url'].replace('{width}', '600').replace('{height}', '800')) # Download image game
 
         if variables.category_name not in variables.games_blacklist: # Add game in list
