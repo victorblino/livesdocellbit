c4df01327175ccee94c8a839b36be43f52fb7ea0
Victor B. Lino <victorbarrosolino@gmail.com>
2023-07-06 09:03:20 -0300

    Commit Fix Twitter Post

--- functions/twitterFunctions.py
+++ functions/twitterFunctions.py
@@ -2,21 +2,23 @@ import tweepy
 from utils import variables
 from functions.botFunctions import printEvent
 
-api = tweepy.Client(
+
+client = tweepy.Client(
     consumer_key=variables.consumer_key, consumer_secret=variables.consumer_secret,
-    access_token=variables.app_key, access_token_secret=variables.secret
+    access_token=variables.twitter_access_token, access_token_secret=variables.twitter_access_secret
 )
-printEvent(True, 'twitter_authenticated')
 
 def postTweet(status):
-    api.create_tweet(text=status)
+    client.create_tweet(text=status)
     printEvent(True, 'twitter_post')
 
 def postTweetWithImage(status, image):
-    api.create_tweet(text=status)
+
+    client.create_tweet(text=status)
     printEvent(True, 'twitter_post_image')
 
 def postReply(status):
-    last_tweet = api.user_timeline(screen_name=variables.bot_user)[0].id
-    api.update_status(status, in_reply_to_status_id=last_tweet)
+
+    last_tweet = client.user_timeline(screen_name=variables.bot_user)[0].id
+    client.update_status(status, in_reply_to_status_id=last_tweet)
     printEvent(True, 'twitter_post_image')
--- utils/variables.py
+++ utils/variables.py
@@ -5,6 +5,8 @@ load_dotenv()
 # Enviroment Variables
 app_key = os.environ.get('TWITCH_APP_KEY')
 app_secret = os.environ.get('TWITCH_APP_SECRET')
+twitter_access_token = os.environ.get('TWITTER_ACCESS_TOKEN')
+twitter_access_secret = os.environ.get('TWITTER_ACCESS_SECRET')
 twitch_bearer = os.environ.get('TWITCH_TWITCH_BEARER')
 streamer_nickname = os.environ.get('TWITCH_STREAMER_NICKNAME')
 consumer_key = os.environ.get('TWITTER_CONSUMER_KEY')
